582a3c44d5216c60e71e5628a9cad722bc1bd94a
JasonZhouE <zhoujason97@gmail.com>
2019-03-24 17:20:56 -0700

    init commit

---
+++ app/Controllers/Chores.php
@@ -0,0 +1,14 @@
+<?php namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class Chroes extends Controller
+{
+	public function index()
+	{
+		return view('welcome_message');
+	}
+
+	//--------------------------------------------------------------------
+
+}
